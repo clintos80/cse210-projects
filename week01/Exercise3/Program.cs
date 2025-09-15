08acf6e8c39d35a5a27a38bdc0baa10d60c4f7ae
clintos80 <selekeoweiclinton13@gmail.com>
2025-09-15 12:33:57 +0100

    week01 assignment Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        
+        Random rnd = new Random();
+        int magic = rnd.Next(1, 101);
+
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != magic)
+        {
+            if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("You guessed it!");
     }
 }
